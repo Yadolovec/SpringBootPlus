9c1e8a698183f8e968436d8bed7a734f0006d772
Havrylets Volodymyr <yadolovec@gmail.com>
2023-01-20 19:50:44 +0200

    test cpmmit

--- SecurityApp/src/main/java/com/BootTraining/SecurityApp/config/SecurityConfig.java
+++ SecurityApp/src/main/java/com/BootTraining/SecurityApp/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     protected void configure(HttpSecurity http) throws Exception{
-        http    .authorizeRequests()
+        http     .authorizeRequests()
                 .antMatchers("/auth/login", "/auth/registration","/error").permitAll()
                 .anyRequest().authenticated()
                 .and()
