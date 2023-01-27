654a7d353d9b9a5f797b0a26f1c161948e10a121
Havrylets Volodymyr <yadolovec@gmail.com>
2023-01-27 14:39:23 +0200

    add login with JWT

--- JWToken/src/main/java/com/some/app/EmptyApp/config/SecurityConfig.java
+++ JWToken/src/main/java/com/some/app/EmptyApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.some.app.EmptyApp.config;
 import com.some.app.EmptyApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -63,5 +64,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 }
--- JWToken/src/main/java/com/some/app/EmptyApp/controllers/AuthController.java
+++ JWToken/src/main/java/com/some/app/EmptyApp/controllers/AuthController.java
@@ -1,9 +1,13 @@
 package com.some.app.EmptyApp.controllers;
 
+import com.some.app.EmptyApp.dto.AuthentificationDTO;
 import com.some.app.EmptyApp.dto.PersonDTO;
 import com.some.app.EmptyApp.security.JWTUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import com.some.app.EmptyApp.models.Person;
@@ -24,13 +28,15 @@ public class AuthController {
     private final PersonValidator personValidator;
     private final JWTUtil jwtUtil;
     private final ModelMapper modelMapper;
+    private final AuthenticationManager authenticationManager;
 
     @Autowired
-    public AuthController(RegistrationService registrationService, PersonValidator personValidator, JWTUtil jwtUtil, ModelMapper modelMapper) {
+    public AuthController(RegistrationService registrationService, PersonValidator personValidator, JWTUtil jwtUtil, ModelMapper modelMapper, AuthenticationManager authenticationManager) {
         this.registrationService = registrationService;
         this.personValidator = personValidator;
         this.jwtUtil = jwtUtil;
         this.modelMapper = modelMapper;
+        this.authenticationManager = authenticationManager;
     }
 
     @GetMapping("/login")
@@ -58,6 +64,22 @@ public class AuthController {
 
         String token = jwtUtil.generateToken(person.getUsername());
         return Map.of("jwt_token", token);
+    }
+
+    @PostMapping("/login")
+    @ResponseBody
+    public Map<String, String> performLogin(@RequestBody AuthentificationDTO authentificationDTO){
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(authentificationDTO.getUsername(), authentificationDTO.getPassword());
+
+        try{
+            authenticationManager.authenticate(authToken);
+        }catch (BadCredentialsException e){
+            return Map.of("message", "bad credentials");
+        }
+
+        String token = jwtUtil.generateToken(authentificationDTO.getUsername());
+        return Map.of("jwt_token", token);
+
     }
 
     public Person convertFromDTO(PersonDTO personDTO){
